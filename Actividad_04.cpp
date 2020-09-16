0ab686b965f9655c73766f4030e6ba6bbfba3f52
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2020-09-16 14:13:36 -0500

    Se agregó la estructura switch y la opción 1 alojada en una función void capturar_enteros()

--- Actividad_04.cpp
+++ Actividad_04.cpp
@@ -2,16 +2,58 @@
 
 using namespace std;
 
+void capturar_enteros(){
+        int enteros[5];
+        float promedio;
+        int suma=0;
+        for (size_t i = 0; i < 5; i++)
+        {
+            cout<<"\nIngrese un numero: ";
+            cin>>enteros[i];
+        }
+        for (size_t i = 0; i < 5; i++)
+        {
+            cout<<enteros[i]<<" "<<endl;
+        }
+        for (size_t i = 0; i < 5; i++)
+        {
+           suma += enteros[i];
+        }
+        promedio = suma/5.0;
+        cout<<"La suma es: "<<suma<<endl;
+        cout<<"El promedio es: "<<promedio<<endl;
+        cout<<"\n";
+    }
+
 int main(){
 
     char opcion; 
-    do{
+    do{     fflush(stdin);
             cout<<" 1) Capturar enteros"<<endl;
             cout<<" 2) Mostrar cadena n veces"<<endl;
             cout<<" 3) Agregar personajes"<<endl;
             cout<<" 0) Salir"<<endl;
             cin>>opcion;
             fflush(stdin);
+
+            switch (opcion)
+            {
+            case '1':   capturar_enteros();
+                
+                break;
+            
+            case '2':
+                break;
+
+            case '3':
+                break;    
+            
+            case '0': cout<<"Saliendo..."<<endl;
+                break;
+
+            default: cout<<"Opcion no valida"<<endl;
+                break;
+            }
     }while(opcion!='0');
 
 }
