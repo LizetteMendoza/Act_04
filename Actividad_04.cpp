6c5cd8b9e826a770bae53fe1e8c93f975bc9e154
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2020-09-16 14:56:00 -0500

    Se agregó la opción 4 encontrada en el encabezado personaje.h

--- Actividad_04.cpp
+++ Actividad_04.cpp
@@ -47,6 +47,7 @@ int main(){
             cout<<" 1) Capturar enteros"<<endl;
             cout<<" 2) Mostrar cadena n veces"<<endl;
             cout<<" 3) Agregar personajes"<<endl;
+            cout<<" 4) Mostrar personajes"<<endl;
             cout<<" 0) Salir"<<endl;
             cin>>opcion;
             fflush(stdin);
@@ -66,6 +67,9 @@ int main(){
 
             case '3':   capturar_personajes();
                 break;    
+
+            case '4':    mostrar_personajes();
+                break;
             
             case '0': cout<<"Saliendo..."<<endl;
                 break;
